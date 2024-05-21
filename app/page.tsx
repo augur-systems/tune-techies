4fc4bf544007b289c5eb83ae70c96963e12fba9e
Adam Goudarzi <adam.goudarzi@augur.systems>
2024-05-21 00:08:19 -0700

    Run prettier to fix boilerplate project

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,34 @@
-"use client";
+'use client'
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+import { useState, useEffect } from 'react'
+import { generateClient } from 'aws-amplify/data'
+import type { Schema } from '@/amplify/data/resource'
+import './../app/app.css'
+import { Amplify } from 'aws-amplify'
+import outputs from '@/amplify_outputs.json'
+import '@aws-amplify/ui-react/styles.css'
 
-Amplify.configure(outputs);
+Amplify.configure(outputs)
 
-const client = generateClient<Schema>();
+const client = generateClient<Schema>()
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([])
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
-    });
+    })
   }
 
   useEffect(() => {
-    listTodos();
-  }, []);
+    listTodos()
+  }, [])
 
   function createTodo() {
     client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
+      content: window.prompt('Todo content'),
+    })
   }
 
   return (
@@ -48,5 +48,5 @@ export default function App() {
         </a>
       </div>
     </main>
-  );
+  )
 }
