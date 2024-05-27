57669c534c25cd692465d7dffcabc022fe50993a
Adam Goudarzi <adam.goudarzi@augur.systems>
2024-05-26 22:03:27 -0700

    Update next.config.js to include the path aliases

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const path = require('path')
+
 // @ts-check
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src') // Adjust based on your project structure
+    return config
+  },
+}
 
 module.exports = nextConfig
