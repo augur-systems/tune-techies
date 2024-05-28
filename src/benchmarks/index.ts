8f1077b7017735e041df9fe13255351caea5147c
Adam Goudarzi <adam.goudarzi@augur.systems>
2024-05-27 19:37:51 -0700

    Setup benchmarking

---
+++ src/benchmarks/index.ts
@@ -0,0 +1,20 @@
+import { readdirSync } from 'fs'
+import { fileURLToPath, pathToFileURL } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+async function runBenchmarks() {
+  const benchmarkDir = __dirname
+  const files = readdirSync(benchmarkDir)
+
+  for (const file of files) {
+    if (file.endsWith('.benchmark.ts')) {
+      const { runBenchmarks } = await import(join(benchmarkDir, file))
+      runBenchmarks()
+    }
+  }
+}
+
+runBenchmarks()
