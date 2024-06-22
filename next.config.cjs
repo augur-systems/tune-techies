fbc0e4e7825c6995ada15361d2362eb2c45ee550
Adam Goudarzi <adam.goudarzi@augur.systems>
2024-06-22 18:39:34 -0400

    Remove webpack from next config

--- next.config.cjs
+++ next.config.cjs
@@ -2,11 +2,6 @@ const path = require('path')
 
 // @ts-check
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src') // Adjust based on your project structure
-    return config
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
